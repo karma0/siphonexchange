a7220fc5251daa1023b40c64eeb8378fe4300989
Bobby <karmma0@gmail.com>
2018-02-05 22:56:08 -0700

    Added configuration retreival

--- application.py
+++ application.py
@@ -1,6 +1,27 @@
 #!flask/bin/python
+
+import os
+
 from flask import Flask
 from flaskrun import flaskrun
+
+
+if not os.path.isfile('config.json'):
+    import boto3
+    import botocore
+
+    BUCKET = os.environ['CONF_BUCKET']
+    KEY = os.environ['CONF_FILE']
+
+    s3 = boto3.resource('s3')
+
+    try:
+        s3.Bucket(BUCKET).download_file(KEY, 'config.json')
+    except botocore.exceptions.ClientError as err:
+        if err.response['Error']['Code'] == '404':
+            print("Object does not exist")
+        else:
+            raise
 
 
 application = Flask(__name__)
