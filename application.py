f50ccd18700a69fa9b3b956d2e66669d67aa425c
Bobby <karmma0@gmail.com>
2018-02-07 13:04:44 -0700

    Setup configuration from env

--- application.py
+++ application.py
@@ -1,41 +1,47 @@
-#!flask/bin/python
+#!flask/bin/python # pylint: disable=missing-docstring
 
 import os
+import json
 
 from flask import Flask
 from flaskrun import flaskrun
 
 
-if not os.path.isfile('config.json'):
-    import boto3
-    import botocore
+# Setup configuration
+CONFIG = {}  # type: dict
+with open("nombot-config.json") as config_handle:
+    CONFIG = json.loads(config_handle.read())
+    for service in CONFIG['api']['services']:
 
-    BUCKET = os.environ['CONF_BUCKET']
-    KEY = os.environ['CONF_FILE']
+        # Configure the credentials
+        service["credentials"] = {
+            "apikey": os.environ.get(f"{service['name'].upper()}_APIKEY"),
+            "secret": os.environ.get(f"{service['name'].upper()}_SECRET")
+        }
 
-    s3 = boto3.resource('s3')
-
-    try:
-        s3.Bucket(BUCKET).download_file(KEY, 'config.json')
-    except botocore.exceptions.ClientError as err:
-        if err.response['Error']['Code'] == '404':
-            print("Object does not exist")
-        else:
-            raise
-
-
-application = Flask(__name__)
+        # Find environment subscriptions
+        for variable in os.environ.keys():
+            priv_key = f"{service['name'].upper()}_SUBSCRIPTION_PRIVATE"
+            if variable == priv_key:
+                msg_type = \
+                    os.environ.get(f"{priv_key}_TYPE", "ws_trade_ticker")
+                if "subscriptions" not in service:
+                    service["subscriptions"] = {}
+                service["subscriptions"][variable] = msg_type
 
 
-@application.route('/', methods=['GET'])
-def get():
+APP = Flask(__name__)
+
+
+@APP.route('/', methods=['GET'])
+def get_index():  # pylint: disable=missing-docstring
     return '{"Output":"Hello World"}'
 
 
-@application.route('/', methods=['POST'])
-def post():
+@APP.route('/', methods=['POST'])
+def post_index():  # pylint: disable=missing-docstring
     return '{"Output":"Hello World"}'
 
 
 if __name__ == '__main__':
-    flaskrun(application)
+    flaskrun(APP)
