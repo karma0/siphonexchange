16a9498ab8b76ed6ec9f4dd5a3280b245c2b474d
Bobby <karmma0@gmail.com>
2018-02-07 20:55:26 -0700

    Breakout config

---
+++ config.py
@@ -0,0 +1,29 @@
+"""Load configuration for the application"""
+import os
+import json
+
+
+CONFIG = {}  # type: dict
+
+# Setup configuration
+with open("nombot-config.json") as config_handle:
+    CONFIG = json.loads(config_handle.read())
+    for service in CONFIG['api']['services']:
+
+        service_name = service['name'].upper()
+
+        # Configure the credentials
+        service["credentials"] = {
+            "apikey": f"{service_name}_APIKEY",
+            "secret": f"{service_name}_SECRET"
+        }
+
+        # Find environment subscriptions
+        for variable in os.environ.keys():
+            priv_key = f"{service_name}_SUBSCRIPTION_PRIVATE"
+            if variable == priv_key:
+                msg_type = \
+                    os.environ.get(f"{priv_key}_TYPE", "ws_trade_ticker")
+                if "subscriptions" not in service:
+                    service["subscriptions"] = {}
+                service["subscriptions"][variable] = msg_type
