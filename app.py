200d925aed24dbd9003f0378a70b13f571e6c80c
Bobby <karma0@gmail.com>
2018-04-19 17:38:30 -0600

    Added a firehose strategy

--- app.py
+++ app.py
@@ -9,23 +9,33 @@ from flaskrun import flaskrun
 from config import CONFIG
 import runner
 
+from strategies.aws import Firehose
 
 application = Flask(__name__)  # pylint: disable=invalid-name
 
 
 @application.route('/', methods=['GET'])
 def get_index():  # pylint: disable=missing-docstring
-    return '{"Output":"Hello World"}'
+    return f"""{"Status":"{PUT_STREAM.status}"}"""
 
 
 @application.route('/', methods=['POST'])
 def post_index():  # pylint: disable=missing-docstring
-    return '{"Output":"Hello World"}'
+    return f"""{"Status":"{PUT_STREAM.status}"}"""
+
+PUT_STREAM = Firehose('siphonexchange-stream')
 
 
 if __name__ == '__main__':
-    # runner.main(config=CONFIG)
-    BOT = Process(target=runner.main, args={'config': CONFIG})
+    BOT = Process(target=runner.main, args={
+        'config': CONFIG,
+        'strategies': [
+            runner.CoinigyStrategy(),
+            runner.PrintResult(),
+            PUT_STREAM
+        ]
+    })
+
     BOT.start()
     flaskrun(application)
     BOT.join()
